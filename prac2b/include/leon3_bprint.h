13eb7c3f492ad85fc114a35ad662de01cb739f58
david_valdelvira <david_valdelvira@d1dd8e34-889a-48a0-8638-679814fbcf02>
2022-03-04 12:41:44 +0000

    Version inicial de la práctica 2b

---
+++ prac2b/include/leon3_bprint.h
@@ -0,0 +1,13 @@
+/*
+ * leon3_bprint.h
+ *
+ *  Created on: Feb 25, 2022
+ *      Author: David
+ */
+
+#ifndef LEON3_BPRINT_H_
+#define LEON3_BPRINT_H_
+#include "leon3_types.h"
+int8_t leon3_print_string(char* str);
+//int8_t leon3_print_uint8(uint8_t i);
+#endif /* LEON3_BPRINT_H_ */
---
+++ prac2b/main.c
@@ -0,0 +1,45 @@
+/*int main(){
+
+	leon3_putchar('P');
+	leon3_putchar('2');
+	leon3_putchar('\n');
+
+	while(!leon3_uart_tx_fifo_is_empty())
+		;
+		return 0;
+
+}*/
+
+int main(){
+	char * pchar="cadena\n0";
+	leon3_print_string(pchar);
+	//leon3_print_uint8(3); leon3_putchar('\n');
+	//leon3_print_uint8(43); leon3_putchar('\n');
+	//leon3_print_uint8(108); leon3_putchar('\n');
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
