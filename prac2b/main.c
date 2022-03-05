674fe0f3adf0cfab7aaf02208d94d2886fadf972
david_valdelvira <david_valdelvira@d1dd8e34-889a-48a0-8638-679814fbcf02>
2022-03-05 14:33:25 +0000

    versión final practica 2b

--- prac2b/main.c
+++ prac2b/main.c
@@ -13,9 +13,10 @@
 int main(){
 	char * pchar="cadena\n0"; //0 al final para que solo imprima "cadena"
 	leon3_print_string(pchar);
-	//leon3_print_uint8(3); leon3_putchar('\n');
-	//leon3_print_uint8(43); leon3_putchar('\n');
-	//leon3_print_uint8(108); leon3_putchar('\n');
+	leon3_print_uint8(3); leon3_putchar('\n');
+	leon3_print_uint8(43); leon3_putchar('\n');
+	leon3_print_uint8(108); leon3_putchar('\n');
+	leon3_putchar('\n');
 	return 0;
 }
 
