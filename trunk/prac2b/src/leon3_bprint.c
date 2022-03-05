04403f9983137f6edd1c6f5766ce8a5b274c4b0d
david_valdelvira <david_valdelvira@d1dd8e34-889a-48a0-8638-679814fbcf02>
2022-03-05 14:33:25 +0000

    versión final practica 2b

--- trunk/prac2b/src/leon3_bprint.c
+++ trunk/prac2b/src/leon3_bprint.c
@@ -8,6 +8,8 @@
 #include "leon3_uart.h"
 #include "leon3_bprint.h"
 
+char uni_char, dec_char, cent_char;	//cadena de caracteres para unidades, decenas y centenas
+
 int8_t leon3_print_string(char* str){
 	
 	int i = 0;
@@ -15,4 +17,42 @@ int8_t leon3_print_string(char* str){
 		leon3_putchar(str[i]);
 		i++;
 	}
+}
+
+int8_t leon3_print_uint8(uint8_t i){
+
+	int uni,dec,cent;	//unidades, decenas y centenas
+
+	if(i<9){
+
+		uni_char = i + '0';
+		leon3_putchar(uni_char);
+
+	}
+
+	if((i>9)&&(i<99)){
+
+		uni = i%10;
+		dec = i/10;
+		dec_char = dec + '0';
+		leon3_putchar(dec_char);
+		uni_char = uni + '0';
+		leon3_putchar(uni_char);
+
+	}
+
+	if((i>99)&&(i<999)){
+
+		cent = i/100;
+		cent_char = cent + '0';
+		leon3_putchar(cent_char);
+		dec = i%10;
+		dec_char = dec + '0';
+		leon3_putchar(dec_char);
+		uni = i%1;
+		uni_char = uni + '0';
+		leon3_putchar(uni_char);
+
+	}
+
 }
