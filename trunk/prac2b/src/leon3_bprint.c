c5e4d84600d1d6ed0a2c2762e9acf3e147f2fe58
david_valdelvira <david_valdelvira@d1dd8e34-889a-48a0-8638-679814fbcf02>
2022-03-05 14:42:21 +0000

    version definitiva prac 2b comentada

--- trunk/prac2b/src/leon3_bprint.c
+++ trunk/prac2b/src/leon3_bprint.c
@@ -23,32 +23,41 @@ int8_t leon3_print_uint8(uint8_t i){
 
 	int uni,dec,cent;	//unidades, decenas y centenas
 
-	if(i<9){
+	if(i<9){	//imprimir numeros del 0 al 9(unidades)
 
+		//valor de la unidad
 		uni_char = i + '0';
 		leon3_putchar(uni_char);
 
 	}
 
-	if((i>9)&&(i<99)){
+	if((i>9)&&(i<99)){	//imprimir numeros del 10 al 99 (decenas y unidades)
 
-		uni = i%10;
+		//valor de la decena
 		dec = i/10;
 		dec_char = dec + '0';
 		leon3_putchar(dec_char);
+
+		//valor de la unidad
+		uni = i%10;
 		uni_char = uni + '0';
 		leon3_putchar(uni_char);
 
 	}
 
-	if((i>99)&&(i<999)){
+	if((i>99)&&(i<999)){	//imprimir numeros del 100 al 999 (centenas, decenas y unidades)
 
+		//valor de la centena
 		cent = i/100;
 		cent_char = cent + '0';
 		leon3_putchar(cent_char);
+
+		//valor de la decena
 		dec = i%10;
 		dec_char = dec + '0';
 		leon3_putchar(dec_char);
+
+		//valor de la unidad
 		uni = i%1;
 		uni_char = uni + '0';
 		leon3_putchar(uni_char);
