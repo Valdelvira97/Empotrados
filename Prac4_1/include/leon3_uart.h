df40ee8f09ffd1df781ace2ba7241a7805c4a0b6
david_valdelvira <david_valdelvira@d1dd8e34-889a-48a0-8638-679814fbcf02>
2022-05-03 15:58:50 +0000

    codigo prac 4_1, leon3_monotonic da errores tras buildear el proyecto entero.

---
+++ Prac4_1/include/leon3_uart.h
@@ -0,0 +1,17 @@
+/*
+ * leon3_uart.h
+ *
+ *  Created on: Feb 19, 2022
+ *      Author: David
+ */
+
+#ifndef LEON3_UART_H_
+#define LEON3_UART_H_
+#include "leon3_types.h"
+int8_t leon3_putchar(char c);
+int8_t leon3_uart_tx_fifo_is_empty();
+#endif //LEON3_UART_H_
+char leon3_getchar();
+void leon3_uart_ctrl_rx_enable();
+void leon3_uart_ctrl_rx_irq_enable();
+void leon3_uart_ctrl_config_rxtx_loop(uint8_t set_rxtxloop);
