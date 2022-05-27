cbb3a62010539a614484e9777d794d6bbe66a652
david_valdelvira <david_valdelvira@d1dd8e34-889a-48a0-8638-679814fbcf02>
2022-05-27 15:30:14 +0000

    Practica 6_1

---
+++ prac6_students/Debug/actuators_drv/src/actuators.d
@@ -0,0 +1,17 @@
+actuators_drv/src/actuators.d actuators_drv/src/actuators.o:  \
+ ../actuators_drv/src/actuators.cpp \
+  /home/osboxes/workspace/prac6_students/actuators_drv/include/public/actuators.h \
+  /home/osboxes/workspace/prac6_students/dataclasses/CDLocation/include/public/cdlocation_iface_v1.h \
+  /home/osboxes/workspace/prac6_students/dataclasses/CDLocation/include/public/cdlocation.h \
+  /home/osboxes/workspace/prac6_students/rtems_osswr/include/public/basic_types.h \
+  /home/osboxes/workspace/prac6_students/rtems_osswr/include/rtems_osswr/leon3_types.h
+
+/home/osboxes/workspace/prac6_students/actuators_drv/include/public/actuators.h:
+
+/home/osboxes/workspace/prac6_students/dataclasses/CDLocation/include/public/cdlocation_iface_v1.h:
+
+/home/osboxes/workspace/prac6_students/dataclasses/CDLocation/include/public/cdlocation.h:
+
+/home/osboxes/workspace/prac6_students/rtems_osswr/include/public/basic_types.h:
+
+/home/osboxes/workspace/prac6_students/rtems_osswr/include/rtems_osswr/leon3_types.h:
---
+++ prac6_students/Debug/components/ccavoidobstacles/src/CCAvoidObstaclesS.d
@@ -0,0 +1,66 @@
+components/ccavoidobstacles/src/CCAvoidObstaclesS.d \
+  components/ccavoidobstacles/src/CCAvoidObstaclesS.o:  \
+ ../components/ccavoidobstacles/src/CCAvoidObstaclesS.cpp \
+  /home/osboxes/workspace/prac6_students/components/ccavoidobstacles/include/public/ccavoidobstacles_iface_v1.h \
+  /home/osboxes/workspace/prac6_students/edroomsl_swr/include/public/edroomsl_iface_v1.h \
+  /home/osboxes/workspace/prac6_students/edroomsl_swr/include/edroomsl/edroomsl.h \
+  /home/osboxes/workspace/prac6_students/config/include/public/config.h \
+  /home/osboxes/workspace/prac6_students/rtems_osswr/include/public/basic_types.h \
+  /home/osboxes/workspace/prac6_students/rtems_osswr/include/rtems_osswr/leon3_types.h \
+  /home/osboxes/workspace/prac6_students/edroombp_swr/include/public/edroombp.h \
+  /home/osboxes/workspace/prac6_students/edroombp_swr/include/edroombp_swr/rtemsapi_4_6/rtems_4_6/edroombp.h \
+  /home/osboxes/workspace/prac6_students/edroombp_swr/include/edroombp_swr/edroomtypes.h \
+  /home/osboxes/workspace/prac6_students/edroomsl_types_swr/include/public/edroomsl_types.h \
+  /home/osboxes/workspace/prac6_students/edroomsl_types_swr/include/edroomsl_types/edroomsl_types.h \
+  /home/osboxes/workspace/prac6_students/edroomsl_swr/include/edroomsl/edroomtc.h \
+  /home/osboxes/workspace/prac6_students/sensors_drv/include/public/sensors_iface_v1.h \
+  /home/osboxes/workspace/prac6_students/sensors_drv/include/public/sensors.h \
+  /home/osboxes/workspace/prac6_students/dataclasses/CDLocation/include/public/cdlocation_iface_v1.h \
+  /home/osboxes/workspace/prac6_students/dataclasses/CDLocation/include/public/cdlocation.h \
+  /home/osboxes/workspace/prac6_students/io_ctrl/include/public/io_ctrl_iface_v1.h \
+  /home/osboxes/workspace/prac6_students/actuators_drv/include/public/actuators_iface_v1.h \
+  /home/osboxes/workspace/prac6_students/actuators_drv/include/public/actuators.h \
+  /home/osboxes/workspace/prac6_students/bprint_swr/include/public/bprint_iface_v1.h \
+  /home/osboxes/workspace/prac6_students/bprint_swr/include/public/bprint.h
+
+/home/osboxes/workspace/prac6_students/components/ccavoidobstacles/include/public/ccavoidobstacles_iface_v1.h:
+
+/home/osboxes/workspace/prac6_students/edroomsl_swr/include/public/edroomsl_iface_v1.h:
+
+/home/osboxes/workspace/prac6_students/edroomsl_swr/include/edroomsl/edroomsl.h:
+
+/home/osboxes/workspace/prac6_students/config/include/public/config.h:
+
+/home/osboxes/workspace/prac6_students/rtems_osswr/include/public/basic_types.h:
+
+/home/osboxes/workspace/prac6_students/rtems_osswr/include/rtems_osswr/leon3_types.h:
+
+/home/osboxes/workspace/prac6_students/edroombp_swr/include/public/edroombp.h:
+
+/home/osboxes/workspace/prac6_students/edroombp_swr/include/edroombp_swr/rtemsapi_4_6/rtems_4_6/edroombp.h:
+
+/home/osboxes/workspace/prac6_students/edroombp_swr/include/edroombp_swr/edroomtypes.h:
+
+/home/osboxes/workspace/prac6_students/edroomsl_types_swr/include/public/edroomsl_types.h:
+
+/home/osboxes/workspace/prac6_students/edroomsl_types_swr/include/edroomsl_types/edroomsl_types.h:
+
+/home/osboxes/workspace/prac6_students/edroomsl_swr/include/edroomsl/edroomtc.h:
+
+/home/osboxes/workspace/prac6_students/sensors_drv/include/public/sensors_iface_v1.h:
+
+/home/osboxes/workspace/prac6_students/sensors_drv/include/public/sensors.h:
+
+/home/osboxes/workspace/prac6_students/dataclasses/CDLocation/include/public/cdlocation_iface_v1.h:
+
+/home/osboxes/workspace/prac6_students/dataclasses/CDLocation/include/public/cdlocation.h:
+
+/home/osboxes/workspace/prac6_students/io_ctrl/include/public/io_ctrl_iface_v1.h:
+
+/home/osboxes/workspace/prac6_students/actuators_drv/include/public/actuators_iface_v1.h:
+
+/home/osboxes/workspace/prac6_students/actuators_drv/include/public/actuators.h:
+
+/home/osboxes/workspace/prac6_students/bprint_swr/include/public/bprint_iface_v1.h:
+
+/home/osboxes/workspace/prac6_students/bprint_swr/include/public/bprint.h:
